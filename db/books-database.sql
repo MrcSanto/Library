0ca6d8cd68a551b9bec13826a4b0cfa01cabab3e
MrcSanto <marco.antonio@santolin.com.br>
2024-09-15 11:36:42 -0300

    entities e db

--- db/books-database.sql
+++ db/books-database.sql
@@ -20,3 +20,34 @@ VALUES ('To Kill a Mockingbird', 'J.B. Lippincott & Co.', 'Pulitzer Prize-winnin
 
 INSERT INTO book (book_name, book_editora, book_comment)
 VALUES ('The Great Gatsby', 'Charles Scribners Sons', 'Classic novel by F. Scott Fitzgerald.');
+
+CREATE TABLE editora (
+    editora_id SERIAL PRIMARY KEY,
+    nome_fantasia VARCHAR(50),
+    razao_social VARCHAR(50)
+);
+CREATE TABLE autor(
+    autor_id SERIAL PRIMARY KEY,
+    autor_nacionalidade VARCHAR(50) NOT NULL
+);
+CREATE TABLE genero(
+    genero_id SERIAL PRIMARY KEY,
+    genero_nome VARCHAR(100) NOT NULL,
+    genero_descricao TEXT NOT NULL
+);
+CREATE TABLE book (
+    book_id SERIAL PRIMARY KEY, -- ISBN
+    book_name VARCHAR(100) NOT NULL,
+    book_editora INTEGER NOT NULL,
+    book_autor INTEGER NOT NULL,
+    book_genero INTEGER NOT NULL,
+    book_remaining INTEGER NOT NULL,
+    book_numPages INTEGER NOT NULL,
+    book_ranking SMALLINT CHECK (book_ranking >= 0 AND book_ranking <= 5),
+    FOREIGN KEY (book_editora) REFERENCES editora(editora_id),
+    FOREIGN KEY (book_autor) REFERENCES autor(autor_id),
+    FOREIGN KEY (book_genero) REFERENCES genero(genero_id)
+);
+
+select * from book
+
