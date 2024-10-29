9307a410cd86369a40822b8af5600d99f065e617
MrcSanto <marco.antonio@santolin.com.br>
2024-10-28 23:30:57 -0300

    rotas privadas

--- src/components/book-catalog/routes/book-routes.ts
+++ src/components/book-catalog/routes/book-routes.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express';
 import {BookController} from "../book-controllers/book-controller";
+import Auth from "../../../middlewares/auth";
 
 const router = Router();
 const bookController = new BookController();
@@ -8,9 +9,9 @@ router.get("/", bookController.getAll);
 router.get("/most-popular", bookController.getMostPopular);
 router.get("/most-recent", bookController.getMostRecent);
 router.get("/:id", bookController.getByid);
-router.post("/", bookController.create);
-router.put("/:id", bookController.update);
-router.patch("/:id", bookController.replace);
-router.delete("/:id", bookController.delete);
+router.post("/", Auth.authenticate, bookController.create);
+router.put("/:id", Auth.authenticate, bookController.update);
+router.patch("/:id", Auth.authenticate, bookController.replace);
+router.delete("/:id", Auth.authenticate, bookController.delete);
 
 export default router;
--- src/components/category-catalog/routes/categoria-routes.ts
+++ src/components/category-catalog/routes/categoria-routes.ts
@@ -1,14 +1,15 @@
 import {Router} from 'express';
 import {CategoriaController} from "../category-controllers/categoria-controller";
+import Auth from "../../../middlewares/auth";
 
 const router = Router();
 const categoriaController = new CategoriaController();
 
 router.get("/", categoriaController.getAll);
 router.get("/:id", categoriaController.getById);
-router.post("/", categoriaController.create);
-router.put("/:id", categoriaController.update);
-router.patch("/:id", categoriaController.replace);
-router.delete("/:id", categoriaController.delete);
+router.post("/", Auth.authenticate, categoriaController.create);
+router.put("/:id", Auth.authenticate, categoriaController.update);
+router.patch("/:id", Auth.authenticate, categoriaController.replace);
+router.delete("/:id", Auth.authenticate, categoriaController.delete);
 
 export default router;
